503d3792719c8609a20ea84fac04906814d6e04e
JOHNIE M <147027375+Johnieaj@users.noreply.github.com>
2024-10-27 22:12:10 +0530

    Add files via upload

---
+++ shaish.py
@@ -0,0 +1,294 @@
+#fuction without arugument and without return type
+#default value
+#operation for sub
+
+def sub():
+    a = 10
+    b = 5
+    c = a-b
+    print(c)
+    
+#fuction call
+sub()
+
+#runtime value
+
+def sub():
+    a=int(input('enter value a:'))
+    b=int(input('enter value b:'))
+    c=a-b
+    print(c)
+    
+#fuction call
+sub()
+
+
+#fuction with argument and without return type
+#default value
+
+def sub(a,b):
+    c = a-b
+    print(c)
+    
+#fuction call
+sub(10,5)
+
+#runtime value
+
+def sub(a,b):
+    c=a-b
+    print(c)
+    
+#fuction call
+a1=int(input('enter value a:'))
+b2=int(input('enter value b:'))
+sub(a1,b2)
+
+
+#fuction without argument and with return type
+#default value
+
+def sub():
+    a=10
+    b=5
+    c=a-b
+    return c
+    
+#fuction call
+x=sub()
+print(x)
+
+#runtime value
+
+def sub():
+    a=int(input('enter value a:'))
+    b=int(input('enter value b:'))
+    c=a-b
+    return c
+    
+#fuction call
+x=sub()
+print(x)
+
+
+#fuction with argument and with return type
+#default value
+
+def sub(a,b):
+    c=a-b
+    return c
+    
+#fuction call
+x=sub(10,5)
+print(x)
+
+#runtime value
+
+def sub(a,b):
+    c=a-b
+    return c
+    
+#fuction call
+
+a1=int(input('enter value a:'))
+b2=int(input('enter value b:'))
+x=sub(a1,b2)
+print(x)
+
+#step 2
+
+#fuction without arugument and without return type
+#default value
+#operation for multi
+
+def multi():
+    a = 10
+    b = 5
+    c = a*b
+    print(c)
+    
+#fuction call
+multi()
+
+#runtime value
+
+def multi():
+    a=int(input('enter value a:'))
+    b=int(input('enter value b:'))
+    c=a*b
+    print(c)
+    
+#fuction call
+multi()
+
+
+#fuction with argument and without return type
+#default value
+
+def multi(a,b):
+    c = a*b
+    print(c)
+    
+#fuction call
+multi(10,5)
+
+#runtime value
+
+def multi(a,b):
+    c=a*b
+    print(c)
+    
+#fuction call
+a1=int(input('enter value a:'))
+b2=int(input('enter value b:'))
+multi(a1,b2)
+
+
+#fuction without argument and with return type
+#default value
+
+def multi():
+    a=10
+    b=5
+    c=a*b
+    return c
+    
+#fuction call
+x=multi()
+print(x)
+
+#runtime value
+
+def multi():
+    a=int(input('enter value a:'))
+    b=int(input('enter value b:'))
+    c=a*b
+    return c
+    
+#fuction call
+x=multi()
+print(x)
+
+
+#fuction with argument and with return type
+#default value
+
+def multi(a,b):
+    c=a*b
+    return c
+    
+#fuction call
+x=multi(10,5)
+print(x)
+
+#runtime value
+
+def multi(a,b):
+    c=a*b
+    return c
+    
+#fuction call
+
+a1=int(input('enter value a:'))
+b2=int(input('enter value b:'))
+x=multi(a1,b2)
+print(x)
+
+#step 3
+#fuction without arugument and without return type
+#default value
+#operation for div
+
+def div():
+    a = 10
+    b = 5
+    c = a/b
+    print(c)
+    
+#fuction call
+div()
+
+#runtime value
+
+def div():
+    a=int(input('enter value a:'))
+    b=int(input('enter value b:'))
+    c=a/b
+    print(c)
+    
+#fuction call
+div()
+
+
+#fuction with argument and without return type
+#default value
+
+def div(a,b):
+    c = a/b
+    print(c)
+    
+#fuction call
+div(10,5)
+
+#runtime value
+
+def div(a,b):
+    c=a/b
+    print(c)
+    
+#fuction call
+a1=int(input('enter value a:'))
+b2=int(input('enter value b:'))
+div(a1,b2)
+
+
+#fuction without argument and with return type
+#default value
+
+def div():
+    a=10
+    b=5
+    c=a/b
+    return c
+    
+#fuction call
+x=div()
+print(x)
+
+#runtime value
+
+def div():
+    a=int(input('enter value a:'))
+    b=int(input('enter value b:'))
+    c=a/b
+    return c
+    
+#fuction call
+x=div()
+print(x)
+
+
+#fuction with argument and with return type
+#default value
+
+def div(a,b):
+    c=a/b
+    return c
+    
+#fuction call
+x=div(10,5)
+print(x)
+
+#runtime value
+
+def div(a,b):
+    c=a/b
+    return c
+    
+#fuction call
+
+a1=int(input('enter value a:'))
+b2=int(input('enter value b:'))
+x=div(a1,b2)
+print(x)
+
